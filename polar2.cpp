6a946c3e6269e77f70ad62f7636cd62bbce650e7
AndreWaehlisch <andre.waehlisch@physik.tu-berlin.de>
2014-10-22 16:04:23 +0200

    add further details for atan2
    Signed-off-by: AndreWaehlisch <andre.waehlisch@physik.tu-berlin.de>

--- polar2.cpp
+++ polar2.cpp
@@ -7,7 +7,7 @@ void cartesian2polar(const double x, const double y)
 	cout << "Ergebnis r = " << sqrt(x*x + y*y) << endl;
 	cout << "Ergebnis phi = " << atan2(y, x) * 180. / M_PI << "°" << endl;
 	
-	// Die Funktion atan2 erwartet als Argumente die beiden Werte der kartesischen Koordinaten x und y (Reihenfolge: y,x) und liefert den arctan in Radian zurück.
+	// Die Funktion atan2 erwartet als Argumente die beiden Werte der kartesischen Koordinaten x und y (Reihenfolge: y,x) und liefert dann arctan(y/x) in Radian (-Pi bis Pi) zurück.
 }
 
 int main()
